6c72fb3673eb2b181ca550d805ad02b572ebd944
Dylan Scott <dylanjgscott@gmail.com>
2014-10-14 14:07:29 +1100

    Worked the example from the lecture slides.

--- src/Genkill.hs
+++ src/Genkill.hs
@@ -10,15 +10,26 @@ data Node a  = Node a
 data Edge a  = Edge (Node a, Node a)
              deriving (Eq, Show)
 
-nodes = [Node "test1", Node "test2", Node "test3"]
-edges = [Edge (nodes!!0, nodes!!1), Edge (nodes!!0, nodes!!2)]
+nodes = [Node "1y", Node "2x", Node "3z", Node "4x", Node "5x", Node "6"]
+edges = [ Edge (nodes!!0, nodes!!1),
+    Edge (nodes!!1, nodes!!2),
+    Edge (nodes!!1, nodes!!3),
+    Edge (nodes!!2, nodes!!5),
+    Edge (nodes!!3, nodes!!4),
+    Edge (nodes!!4, nodes!!5) ]
 graph = Graph nodes edges
 
-gen :: Eq a => Graph a -> Node a -> [Node a]
-gen g n = [n]
+gen :: Graph [Char] -> Node [Char] -> [Node [Char]]
+gen g@(Graph ns es) n@(Node x) = if length x == 2 then [n] else []
 
-kill :: Eq a => Graph a -> Node a -> [Node a]
-kill g n = []
+kill :: Graph [Char] -> Node [Char] -> [Node [Char]]
+kill g@(Graph ns es) n@(Node x) = if length x == 2 then overlapping (x!!1) (delete n ns) else []
+    where
+        overlapping :: Char -> [Node [Char]] -> [Node [Char]]
+        overlapping _ [] = []
+        overlapping c (n@(Node x):ns)
+            | length x == 2 && x!!1 == c = n : overlapping c ns
+            | otherwise = overlapping c ns
 
 genkill
     :: Eq a
@@ -26,16 +37,29 @@ genkill
     -> (Graph a -> Node a -> [Node a])
     -> (Graph a -> Node a -> [Node a])
     -> [(Node a, [Node a], [Node a])]
-genkill g@(Graph ns es) gen kill = [(n, labelin g n, labelout g n) | n <- ns]
+genkill g@(Graph ns es) gen kill = [(n, labelin g gen kill n, labelout g gen kill n) | n <- ns]
 
-labelin :: Eq a => Graph a -> Node a -> [Node a]
-labelin g@(Graph ns es) n = foldl union [] [labelout g p | p <- predecessors es n]
+labelin
+    :: Eq a
+    => Graph a
+    -> (Graph a -> Node a -> [Node a])
+    -> (Graph a -> Node a -> [Node a])
+    -> Node a
+    -> [Node a]
+labelin g@(Graph ns es) gen kill n = foldl union [] [labelout g gen kill p | p <- pred es n]
+    where
+    pred :: Eq a => [Edge a] -> Node a -> [Node a]
+    pred [] _ = []
+    pred ((Edge (src, dst)):es) n
+        | n == dst = src : pred es n
+        | otherwise = pred es n
 
-labelout :: Eq a => Graph a -> Node a -> [Node a]
-labelout g@(Graph ns es) n = union (gen g n) ((labelin g n) \\ (kill g n))
+labelout
+    :: Eq a
+    => Graph a
+    -> (Graph a -> Node a -> [Node a])
+    -> (Graph a -> Node a -> [Node a])
+    -> Node a
+    -> [Node a]
+labelout g@(Graph ns es) gen kill n = union (gen g n) ((labelin g gen kill n) \\ (kill g n))
 
-predecessors :: Eq a => [Edge a] -> Node a -> [Node a]
-predecessors [] _ = []
-predecessors ((Edge (src, dst)):es) n
-    | n == dst = src : predecessors es n
-    | otherwise = predecessors es n
