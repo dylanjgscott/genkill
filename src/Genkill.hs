ff0b25f3e3c7ca5e2bcd62ea02ea821929899f4b
Dylan Scott <dylanjgscott@gmail.com>
2014-10-21 20:42:21 +1100

    Removed testing code and added some comments.

--- src/Genkill.hs
+++ src/Genkill.hs
@@ -6,7 +6,6 @@ import Data.List
 
 data Graph a = Graph [Node a] [Edge a]
              deriving (Eq, Show)
-
 data Node a  = Node a
              deriving (Eq, Show)
 data Edge a  = Edge (Node a, Node a)
@@ -15,46 +14,27 @@ data Edge a  = Edge (Node a, Node a)
 type Gen a b = Graph a -> Node a -> [b]
 type Kill a b = Graph a -> Node a -> [b]
 
-nodes = [Node "1y", Node "2x", Node "3z", Node "4x", Node "5x", Node "6"]
-edges = [ Edge (nodes!!0, nodes!!1),
-    Edge (nodes!!1, nodes!!2),
-    Edge (nodes!!1, nodes!!3),
-    Edge (nodes!!2, nodes!!5),
-    Edge (nodes!!3, nodes!!4),
-    Edge (nodes!!4, nodes!!5) ]
-graph = Graph nodes edges
-
-gen :: Gen String (Node String)
-gen g@(Graph ns es) n@(Node x) = if length x == 2 then [n] else []
-
-kill :: Kill String (Node String)
-kill g@(Graph ns es) n@(Node x) =
-    if length x == 2
-    then overlapping (x!!1) (delete n ns)
-    else []
-        where
-            overlapping :: Char -> [Node [Char]] -> [Node [Char]]
-            overlapping _ [] = []
-            overlapping c (n@(Node x):ns)
-                | length x == 2 && x!!1 == c = n : overlapping c ns
-                | otherwise = overlapping c ns
-
+-- Generate all the in and out labels for all the nodes in a graph.
 genkill
     :: forall a b . (Eq a, Eq b)
     => Graph a
     -> Gen a b
     -> Kill a b
     -> [(Node a, [b], [b])]
+
 genkill g@(Graph ns es) gen kill = [(n, labelin n, labelout n) | n <- ns]
 
     where
 
+    -- Generates all the labels coming in to a node.
     labelin :: Node a -> [b]
     labelin n = foldl union [] [labelout p | p <- pred es n]
 
+    -- Generates all the labels coming out out of a node.
     labelout :: Node a -> [b]
     labelout n = union (gen g n) ((labelin n) \\ (kill g n))
 
+    -- Lists all the nodes with an edge ending at the given node.
     pred :: [Edge a] -> Node a -> [Node a]
     pred [] _ = []
     pred ((Edge (src, dst)):es) n
