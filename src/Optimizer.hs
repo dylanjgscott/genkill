b9665b2fed6db0f3e9c665dae47d46d39d1de3f5
gobnat <rafael.mazzoldi@gmail.com>
2014-10-26 01:38:02 +1100

    blank optimizer

--- src/Optimizer.hs
+++ src/Optimizer.hs
@@ -5,7 +5,7 @@ import System.Environment
 import System.Exit
 import System.Console.GetOpt
 import Data.Char
-
+import Data.List
 
 
 
@@ -19,5 +19,21 @@ options = []
 main :: IO ()
 main = do
     [filename, options]    <- getArgs
+
+    if (isInfixOf "u" options)
+        then print "Remove unreachable code."   -- Run U
+        else print "Leaving unreachable code."
+
+    if (isInfixOf "d" options)
+        then print "Remove dead code."          -- Run dead
+        else print "Leaving dead loads."
+
+
+    if (isInfixOf "l" options)                  -- Run l
+        then print "Remove redundant loads."
+        else print "Leaving redundant loads."
+
+
+
     print $ show filename
     print $ show options
